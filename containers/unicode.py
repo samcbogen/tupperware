d07be8efe9bd127089f8275d2ca6479097a06f03
samcbogen <Samuel.Bogen.23@cmc.edu>
2021-04-06 22:08:33 -0700

    add unicode

--- containers/unicode.py
+++ containers/unicode.py
@@ -12,7 +12,8 @@ class NormalizedStr:
     >>> 'César' in 'César Chávez'
     False
 
-    The two strings to the right of the in keyword above are equal *semantically*,
+    The two strings to
+    the right of the in keyword above are equal *semantically*,
     but not equal *representationally*.
     In particular, the first is in NFC form, and the second is in NFD form.
     The purpose of this class is to automatically normalize our strings for us,
@@ -20,25 +21,34 @@ class NormalizedStr:
     '''
 
     def __init__(self, text, normal_form='NFC'):
-        pass
+        self.text = unicodedata.normalize(normal_form, text)
+        self.normal_form = normal_form
 
     def __repr__(self):
         '''
-        The string returned by the __repr__ function should be valid python code
-        that can be substituted directly into the python interpreter to reproduce an equivalent object.
+        The string returned
+        by the __repr__ function should be valid python code
+        that can be substituted directly into the python
+        interpreter to reproduce an equivalent object.
         '''
+        return "NormalizedStr('" + str(self.text) + "', '" + \
+            str(self.normal_form) + "')"
 
     def __str__(self):
         '''
-        This functions converts the NormalizedStr into a regular string object.
-        The output is similar, but not exactly the same, as the __repr__ function.
+        This functions converts the
+        NormalizedStr into a regular string object.
+        The output
+        is similar, but not exactly the same, as the __repr__ function.
         '''
+        return str(self.text)
 
     def __len__(self):
         '''
         Returns the length of the string.
         The expression `len(a)` desugars to a.__len__().
         '''
+        return len(self.text)
 
     def __contains__(self, substr):
         '''
@@ -46,24 +56,33 @@ class NormalizedStr:
         The expression `a in b` desugars to `b.__contains__(a)`.
 
         HINT:
-        You should normalize the `substr` variable to ensure that the comparison is done semantically and not syntactically.
+        You should normalize the `substr` variable
+        to ensure that the comparison is done
+        semantically and not syntactically.
         '''
+        normal_substr = unicodedata.normalize(self.normal_form, substr)
+        return normal_substr in self.text
 
     def __getitem__(self, index):
         '''
         Returns the character at position `index`.
         The expression `a[b]` desugars to `a.__getitem__(b)`.
         '''
+        return self.text[index]
 
     def lower(self):
         '''
         Returns a copy in the same normalized form, but lower case.
         '''
+        lower_form = str(self.text).lower()
+        return lower_form
 
     def upper(self):
         '''
         Returns a copy in the same normalized form, but upper case.
         '''
+        upper_form = str(self.text).upper()
+        return upper_form
 
     def __add__(self, b):
         '''
@@ -71,14 +90,35 @@ class NormalizedStr:
         The expression `a + b` gets desugared into `a.__add__(b)`.
 
         HINT:
-        The addition of two normalized strings is not guaranteed to stay normalized.
-        Therefore, you must renormalize the strings after adding them together.
+        The addition of two normalized
+        strings is not guaranteed to stay normalized.
+        Therefore, you must renormalize
+        the strings after adding them together.
         '''
+        add = unicodedata.normalize(self.normal_form, self.text + str(b))
+        return NormalizedStr(add)
 
     def __iter__(self):
         '''
         HINT:
-        Recall that the __iter__ method returns a class, which is the iterator object.
-        You'll need to define your own iterator class with the appropriate magic methods,
+        Recall that the __iter__ method
+        returns a class, which is the iterator object.
+        You'll need to define
+        your own iterator class with the appropriate magic methods,
         and return an instance of that class here.
         '''
+        return NormalizedStrIter(self.text)
+
+
+class NormalizedStrIter:
+    def __init__(self, text):
+        self.text = text
+        self.i = 0
+
+    def __next__(self):
+        if len(self.text) <= self.i:
+            raise StopIteration
+        else:
+            count = self.text[self.i]
+            self.i += 1
+            return count

e0c80a1e9cffb630642b54cdc8ac6c505c6f24f3
samcbogen <70249466+samcbogen@users.noreply.github.com>
2021-04-26 22:04:01 -0700

    Update Heap.py

--- containers/Heap.py
+++ containers/Heap.py
@@ -1,10 +1,14 @@
 '''
 This file implements the Heap data structure as a subclass of the BinaryTree.
-The book implements Heaps using an *implicit* tree with an *explicit* vector implementation,
-so the code in the book is likely to be less helpful than the code for the other data structures.
-The book's implementation is the traditional implementation because it has a faster constant factor
+The book implements Heaps using an *implicit* tree
+with an *explicit* vector implementation,
+so the code in the book is likely to be less helpful than
+the code for the other data structures.
+The book's implementation is the
+traditional implementation because it has a faster constant factor
 (but the same asymptotics).
-This homework is using an explicit tree implementation to help you get more practice with OOP-style programming and classes.
+This homework is using an explicit tree implementation
+to help you get more practice with OOP-style programming and classes.
 '''
 
 from containers.BinaryTree import BinaryTree, Node
@@ -33,13 +37,15 @@ class Heap(BinaryTree):
         Notice that in the BinaryTree class,
         we defined a __str__ function,
         but not a __repr__ function.
-        Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
+        Recall that the __repr__ function should return a string 
+        that can be used to recreate a valid instance of the class.
         Thus, if you create a variable using the command Heap([1,2,3])
         it's __repr__ will return "Heap([1,2,3])"
 
         For the Heap, type(self).__name__ will be the string "Heap",
         but for the AVLTree, this expression will be "AVLTree".
-        Using this expression ensures that all subclasses of Heap will have a correct implementation of __repr__,
+        Using this expression ensures that all subclasses of Heap 
+        will have a correct implementation of __repr__,
         and that they won't have to reimplement it.
         '''
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
@@ -47,9 +53,11 @@ class Heap(BinaryTree):
     def is_heap_satisfied(self):
         '''
         Whenever you implement a data structure,
-        the first thing to do is to implement a function that checks whether
+        the first thing to do is to
+        implement a function that checks whether
         the structure obeys all of its laws.
-        This makes it possible to automatically test whether insert/delete functions
+        This makes it possible to
+        automatically test whether insert/delete functions
         are actually working.
         '''
         if self.root:
@@ -87,15 +95,20 @@ class Heap(BinaryTree):
 
         HINT:
         The pseudo code is
-        1. Find the next position in the tree using the binary representation of the total number of nodes
-            1. You will have to explicitly store the size of your heap in a variable (rather than compute it) to maintain the O(log n) runtime
-            1. See https://stackoverflow.com/questions/18241192/implement-heap-using-a-binary-tree for hints
+        1. Find the next position in the tree
+        using the binary representation of the total number of nodes
+            1. You will have to explicitly store the size of your heap in a variable
+            (rather than compute it) to maintain the O(log n) runtime
+            1. See https://stackoverflow.com/questions/18241192/
+            implement-heap-using-a-binary-tree for hints
         1. Add `value` into the next position
-        1. Recursively swap value with its parent until the heap property is satisfied
+        1. Recursively swap value with its parent until the heap
+        property is satisfied
 
         HINT:
         Create a @staticmethod helper function,
-        following the same pattern used in the BST and AVLTree insert functions.
+        following the same pattern used in the
+        BST and AVLTree insert functions.
         '''
         if self.root:
             num_nodes = self.__len__()
@@ -115,7 +128,8 @@ class Heap(BinaryTree):
             if not node.right:
                 node.right = Node(value)
             else:
-                node.right = Heap._insert(node.right, value, insertion_path[1:])
+                node.right = Heap._insert(node.right, 
+                                          value, insertion_path[1:])
         if insertion_path[0] == '0':
             if node.left.value < node.value:
                 x = node.value
@@ -163,13 +177,18 @@ class Heap(BinaryTree):
         HINT:
         The pseudocode is
         1. remove the bottom right node from the tree
-        2. replace the root node with what was formerly the bottom right
-        3. "trickle down" the root node: recursively swap it with its largest child until the heap property is satisfied
+        2. replace the root node with what was formerly
+        the bottom right
+        3. "trickle down" the root node: recursively swap it with
+        its largest child until the heap property is satisfied
 
         HINT:
-        I created two @staticmethod helper functions: _remove_bottom_right and _trickle.
-        It's possible to do it with only a single helper (or no helper at all),
-        but I personally found dividing up the code into two made the most sense.
+        I created two @staticmethod helper functions:
+        _remove_bottom_right and _trickle.
+        It's possible to do it with only a single helper
+        (or no helper at all),
+        but I personally found dividing up the code into
+        two made the most sense.
         '''
         if not self.root:
             pass
